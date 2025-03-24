437f12632938e9caa75871c077cfc2400117a91f
Jonathas Dias <83734773+wCosmic@users.noreply.github.com>
2025-03-24 22:49:27 +0000

    NivelMestre_Xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
 
-#include <stdio.h>
+void moverrainha(int movirain){
+    if (movirain > 0) {
+        printf("Movendo para esquerda\n");
+        moverrainha(movirain - 1);
+    }
+}
+
+void movertorre(int movitower){
+    if(movitower > 0) {
+        printf("Movendo para Direita\n");
+        movertorre(movitower - 1);
+    }
+}
+
+void moverbispo(int movibispo){
+    if(movibispo > 0) {
+        printf("Movendo para Cima, Direita\n");
+        moverbispo(movibispo - 1);
+    }
+}
 
 int main(){
 
 //variaveis - peças do tabuleiro
-int torre = 1;
-int bispo = 1;
-int rainha = 1;
+int torre = 1, bispo = 1, rainha = 1, cavalo;
+
+
+//Variavel de movimentação
+int pocoto = 1;
 
 //Criando variaveis para menu
 int menu, menu2;
@@ -28,37 +49,40 @@ printf("Quais peças voce quer mover?\n");
 printf("1 - Torre.\n");
 printf("2 - Bispo.\n");
 printf("3 - Rainha.\n");
-printf("4 - Sair.\n");
+printf("4 - Cavalo.\n");
+printf("5 - Sair.\n");
 scanf("%d", &menu2);
 
     switch (menu2)
     {
-    case 1: // movendo a torre com While
-    while (torre <= 5)
+    case 1: // movendo a torre com recursividade
+    movertorre(5);
+
+    break;
+
+    case 2: // movendo o bispo com recursividade
+       moverbispo(5);
+    
+    break;
+
+    case 3: // movendo a rainha com recursividade
+        moverrainha(8);
+
+
+    break;
+
+    case 4: // movendo o cavalo com loop aninhado
+    while (pocoto--)
     {
-        printf("Movendo para direita\n", torre);
-        torre++;
-    }
-    break;
-
-    case 2: // movendo o bispo com o DoWhile
-    do { 
-        printf("Movendo para Cima, Direita\n", bispo);
-        bispo++;
-    
-        } while (bispo <= 5 );
-
-        {
-    }
-    break;
-    
-    case 3: // movendo a rainha com o For
-        for (rainha = 0; rainha < 8; rainha++)
+        for (cavalo = 0; cavalo < 2; cavalo++){
+            printf("Movendo para baixo\n");
+        }
         printf("Movendo para esquerda\n");
+    }
 
     break;
 
-    case 4: // Opcao para sair do jogo
+    case 5: // Opcao para sair do jogo
         printf("Saindo do jogo...");
         break;
     default: //caso alguma opcao seja invalida
